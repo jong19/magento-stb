4008e46dfaca1616ca1f08f06a772cd2cba5a2d7
jong19 <jhongdelfino@gmail.com>
2024-08-09 01:23:21 +0800

    changes from task-001 (#1)

--- tests/user-account/CreateAccount.test.ts
+++ tests/user-account/CreateAccount.test.ts
@@ -9,7 +9,6 @@ test.describe.configure({
 })
 
 
-
 test.describe('User Account', async() => {
   test('Create Account', async({landingPageFixture, createAccountPageFixture, homePageFixture, page}) => {
     // await landingPageFixture.isInLandingPage()
@@ -20,39 +19,11 @@ test.describe('User Account', async() => {
     await createAccountPageFixture.isAccountSuccessfullyCreated()
 
     await page.context().storageState({path : "./LoginAuth.json"})
- 
-
-
-  })
-
-})
-
-
-test.describe('Wishlist', async() => {
-  test('Add to Wishlist', async({landingPageFixture, loginPageFixture, homePageFixture, listingDetailPageFixture, listingPageFixture, wishListPageFixture}) => {
-
-    await homePageFixture.toProductListingPage1()
-    await homePageFixture.isInProductListing1()
-
-    await listingPageFixture.selectProduct1()
-    
-    await listingDetailPageFixture.addItemToWishList()
-
-
-    await homePageFixture.toWishListPage()
-
-    await wishListPageFixture.isInWishListPage()
-    await wishListPageFixture.verifyWishListItemAdded()
-
-
-
 
 
 
   })
 
-
 })
-
 
 
